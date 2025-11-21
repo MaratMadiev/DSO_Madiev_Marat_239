fdcea20083dce13ec9adfb897497b68526c6a218
MaratMadiev <59918234+MaratMadiev@users.noreply.github.com>
2025-11-21 20:40:54 +0300

    fix: db default value

--- app/database.py
+++ app/database.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()
 
 # Безопасное получение URL базы данных
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
